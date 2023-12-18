6f63443e76f385903fb01d9b5da7b028eb65e526
Mukund Kumar <mukund.kr09@gmail.com>
2023-12-18 18:55:34 +0530

    Commit by kunal

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Teri Hi Galiyon Mein Awara Shaam Hai",
+      artist: "Meet Bros ft. Piyush Mehroliyaa",
+      img: "Aawara Shaam",
+      src: "Aawara Shaam"
+    },
+    {
+      name: "Ab Na Phir Se - Lyrical",
+      artist: "Yasser Desai ",
+      img: "Ab Na Phir Se",
+      src: "Ab Na Phir Se"
+    },
+    {
+      name: "Lyrical | Bande Hain Hum Uske",
+      artist: "Shivam Mahadevan, Anish Sharma",
+      img: "Bande Hain Hum Uske",
+      src: "Bande Hain Hum Uske"
+    },
+    {
+      name: "Dil Jhoom | Gadar 2 ",
+      artist: "Arijit Singh & Mithoon",
+      img: "Dil Jhoom  Gadar 2",
+      src: "Dil Jhoom  Gadar 2"
+    },
+    {
+      name: "Dil(Lyrical):Ek Villain Returns",
+      artist: "Raghav Chaitanya",
+      img: "Dil(Lyrical)",
+      src: "Dil(Lyrical)"
+    },
+    {
+      name: "IJAZAT LYRICS One Night Stand (2016)",
+      artist: "Arijit Singh",
+      img: "IJAZAT LYRICS",
+      src: "IJAZAT LYRICS"
+    },
+    {
+        name: "Kaun Tujhe M.S. Dhoni -The Untold Story",
+        artist: "Amaal Mallik Palak",
+        img: "KAUN TUJHE",
+        src: "KAUN TUJHE"
+      },
+      {
+        name: "Rasiya - Lyric",
+        artist: "Tushar Joshi, Shreya Ghoshal",
+        img: "Rasiya - Lyric",
+        src: "Rasiya - Lyric"
+      },
+      {
+        name: "Sau sau awaazein maare akhiyan (Slowed Reverb)",
+        artist: "Jyotica Tangri",
+        img: "Sau sau awaazein",
+        src: "Sau sau awaazein"
+      },
+      {
+        name: "Suna Hai - Sanak",
+        artist: "Jubin Nautiyal",
+        img: "Suna Hai - Sanak",
+        src: "Suna Hai - Sanak"
+      },
+      {
+        name: "Thoda Thoda Pyaar",
+        artist: "Stebin Ben",
+        img: "Thoda Thoda Pyaar",
+        src: "Thoda Thoda Pyaar"
+      },
+      {
+        name: "Tu Samne Baitha Rahe ",
+        artist: "Shambhavi thakur (Abhi Dutt)",
+        img: "Tu Samne Baitha Rahe",
+        src: "Tu Samne Baitha Rahe"
+      },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
